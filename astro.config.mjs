82ae7f3b8a350f0a50fcb42acebd09cb26449b06
TiagoJacinto <tiagojacintodev@gmail.com>
2026-05-23 10:18:13 +0100

    chore: migrate from npm to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   output: 'static',
   adapter: vercel(),
   markdown: {
